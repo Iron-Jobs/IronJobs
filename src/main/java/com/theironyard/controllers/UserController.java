9d299fca9518d0cc7d7efacd2b1bb3082c23d351
Eduardo Jimenez <EddyJ@Eduardos-MacBook-Pro.local>
2016-08-03 15:15:22 -0700

    work done on controller

--- src/main/java/com/theironyard/controllers/UserController.java
+++ src/main/java/com/theironyard/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.theironyard.controllers;
 
+import com.theironyard.services.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class UserController {
-    
+
+    @Autowired
+    UserRepository userRepository;
+
+
 }
