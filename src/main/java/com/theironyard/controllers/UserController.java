58f3f67d71ac889925ebc2e3c63efb9205c00cf4
Eduardo Jimenez <EddyJ@Eduardos-MacBook-Pro.local>
2016-08-03 15:05:10 -0700

    added userController

---
+++ src/main/java/com/theironyard/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.theironyard.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by EddyJ on 8/3/16.
+ */
+@RestController
+@CrossOrigin
+public class UserController {
+    
+}
