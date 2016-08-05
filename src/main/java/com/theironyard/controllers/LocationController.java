be4de9ee0386d4169e16a36c91fc6e6a92bd0f4f
Eduardo Jimenez <EddyJ@Eduardos-MacBook-Pro.local>
2016-08-05 10:12:09 -0700

    added location controller

---
+++ src/main/java/com/theironyard/controllers/LocationController.java
@@ -0,0 +1,35 @@
+package com.theironyard.controllers;
+
+import com.theironyard.entities.Location;
+import com.theironyard.services.LocationRepository;
+import com.theironyard.services.PostingRepository;
+import com.theironyard.services.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by EddyJ on 8/5/16.
+ */
+@CrossOrigin
+@RestController
+public class LocationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostingRepository postingRepository;
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @RequestMapping(path = "/locations", method = RequestMethod.GET)
+    public List<Location> getAllLocations(){
+        return locationRepository.findAll();
+    }
+}
